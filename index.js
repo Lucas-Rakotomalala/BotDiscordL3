4af2ebd8bedd3bac2f48b664b8129ac657809e40
Lucas Rakotomalala <lucas.rakotomalala@etu.univ-lyon1.fr>
2021-01-27 09:34:05 +0100

    Ajout commande error

--- index.js
+++ index.js
@@ -481,6 +481,10 @@ client.on('message', message => {
         // Send the attachment in the message channel with a content
         message.channel.send(`${message.author}, Voici votre cours`, attachment);
     }
+
+    else {
+        message.channel.send(`${message.author}, Y'a pas de question bêtes juste des étudiants. `);
+    }
 });
 
 // Log our bot in using the token from https://discord.com/developers/applications
